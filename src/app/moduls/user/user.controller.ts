cb33626220b7bb639e5cdd51219f1e0b8e035a49
hrSajib <harun15-4293@diu.edu.bd>
2025-03-05 10:16:20 +0600

    learned transaction rollback using sessions

--- src/app/moduls/user/user.controller.ts
+++ src/app/moduls/user/user.controller.ts
@@ -3,21 +3,25 @@ import sendResponse from "../../utilitties/sendResponse";
 import { UserService } from "./user.service";
 import httpStatus from 'http-status'
 import { tryCatchAsync } from "../../utilitties/tryCatch";
+import { AppError } from "../../errors/errors";
 
 
-const createStudent : RequestHandler = tryCatchAsync(async (req, res, next) => {
+const createStudent: RequestHandler = tryCatchAsync(async (req, res, next) => {
 
-    // Call service function to send this data
-    const result = await UserService.createStudentIntoDB(req.body.student.password,req.body.student);
+    const result = await UserService.createStudentIntoDB(req.body.student.password, req.body.student);
 
-    sendResponse(res,{ //using sendResponse a common utility generic function for sending response
+    if (!result) {
+        throw new AppError(httpStatus.BAD_REQUEST,"Student creation failed"); // This will be caught by `tryCatchAsync`
+    }
+
+    sendResponse(res, {
         success: true,
         statusCode: httpStatus.OK,
-        message: "Student created successfully..",
+        message: "Student created successfully",
         data: result
     });
-
 });
+
 
 
 
--- src/app/moduls/user/user.service.ts
+++ src/app/moduls/user/user.service.ts
@@ -1,39 +1,72 @@
+import mongoose from "mongoose";
 import config from "../../config";
+import { AppError } from "../../errors/errors";
 import { Student } from "../student/student.interface";
 import { StudentModel } from "../student/student.model";
 import { TUser } from "./user.interface";
 import UserModel from "./user.model";
-import { generateStudentId, generateStudentId_1 } from "./user.utilities";
+import { generateStudentId_1 } from "./user.utilities";
+import HttpStatus from "http-status";
 
 
 
 const createStudentIntoDB = async(password: string, studentData: Student)=>{
 
-    password = config.default_pass as string || password
-
-    const studentId = await generateStudentId_1(studentData.admissionSemester);
-
-    const userData: Partial<TUser> = {
-        role: "student",
-        password, 
-        id: studentId
-    };
 
 
-// create user 
-    const result = await UserModel.create(userData);
+    const session = await mongoose.startSession();
 
 
-// create student 
-    if(Object.keys(result).length){
+    try{
 
-        studentData.id = result.id;
-        studentData.user = result._id;
+        session.startTransaction();
 
-        const newStudent = await StudentModel.create(studentData) ;
-        return newStudent;
 
+        password = config.default_pass as string || password
+
+        const studentId = await generateStudentId_1(studentData.admissionSemester);
+    
+        const userData: Partial<TUser> = {
+            role: "student",
+            password, 
+            id: studentId
+        };
+
+        // create user  (Transaction 1)
+        const result = await UserModel.create([userData], {session});
+
+
+        // create student 
+            if(!result.length){
+                throw new AppError(HttpStatus.BAD_REQUEST,"Failed to create user!" )
+            }
+            else{
+
+                studentData.id = result[0].id;
+                studentData.user = result[0]._id;
+
+                // ( Transaction 2 )
+                const newStudent = await StudentModel.create([studentData],{session}) ;
+
+
+                if(!newStudent){
+                    throw new AppError(HttpStatus.BAD_REQUEST,"Failed to create student!" )
+                }
+
+
+                await session.commitTransaction()
+                await session.endSession()
+
+                return newStudent;
+
+            }
+    }catch(err){
+        await session.abortTransaction()
+        await session.endSession()
     }
+
+
+
 
 }
 
